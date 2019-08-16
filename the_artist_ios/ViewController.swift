c113a63b1cc55078d73568432d1ccec813eeec2e
rov <valadares.thel@gmail.com>
2019-08-15 21:45:19 -0300

    Creates project

---
+++ the_artist_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  the_artist_ios
+//
+//  Created by Raoni de Oliveira Valadares on 8/15/19.
+//  Copyright © 2019 rov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
