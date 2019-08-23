e46f75de6d35eb3adce92f8771aaf1e22a4d5ce6
rov <raonivaladares@avenuecom.com>
2019-08-23 15:03:31 -0300

    Creates networkPlaform folder

--- the_artist_ios/Domain/ArtModel.swift
+++ the_artist_ios/Domain/ArtModel.swift
@@ -1,3 +1,10 @@
 struct ArtModel {
+    let remoteID: Int //objectID
+    let title: String
+    let objectName: String // ps: Chair, Ball, Vase
+    let period: String
+    let culture: String
     
+    let primaryImage: String
+    let primaryImageSmall: String
 }
---
+++ the_artist_ios/NetworkPlataform/Request/Request.swift
@@ -0,0 +1,16 @@
+//import Alamofire
+
+//TODO: Avaliate if Requestable is needed.
+//protocol Requestable {
+//    var path: URL { get }
+//    var method: HTTPMethod { get }
+//    var encoding: ParameterEncoding { get }
+//    var parameters: [String: Any]? { get }
+//}
+//
+//struct Request: Requestable {
+//    let path: URL
+//    let method: HTTPMethod
+//    let encoding: ParameterEncoding
+//    let parameters: [String : Any]?
+//}
